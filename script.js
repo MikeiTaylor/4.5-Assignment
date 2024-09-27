b9dee1b649e4a4ca6681f11ad15080da6916f072
MikeiTaylor <143642773+MikeiTaylor@users.noreply.github.com>
2024-09-27 18:10:26 -0500

    New Repository Update

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+
+const hp=100;
+
+if(hp > 75) {
+console.log(`Looking good feeling great`);
+} else if (hp < 50) {
+console.log(`Not feeling so well`);
+} else{
+console.log(`Got to be a little more careful`);
+}
+
+//Normally//
+
+const name = `Michael`;
+const age = 23;
+
+console.log(“Hey there, I’m ” + name + “ and I’m” + age + “ years old.”;
+
